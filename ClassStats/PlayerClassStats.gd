3ec9e1955e20f4643c22813c724c77d9376d4d86
apiotrowski255 <awesomea900@gmail.com>
2022-12-10 22:03:34 +1100

    adding in sub menus

--- ClassStats/PlayerClassStats.gd
+++ ClassStats/PlayerClassStats.gd
@@ -3,9 +3,9 @@ class_name PlayerClassStats
 
 const MAX_EXPERIENCE := 100
 
+export(Resource) var inventory : Resource
+
 var experience := 0 setget set_experience
-
-
 
 func set_experience(value : int) -> void:
 	experience = value
--- UI/ScrollList.gd
+++ UI/ScrollList.gd
@@ -1,16 +1,14 @@
 extends PanelContainer
 class_name ScrollList
 
-var elizabethStats : PlayerClassStats = ReferenceStash.elizabethStats
-
 const ResourceButtonScene := preload("res://UI/ResourceButton.tscn")
 
 onready var scroll_container: ScrollContainer = $"%ScrollContainer"
 onready var button_container: VBoxContainer = $"%ButtonContainer"
 
-func _ready() -> void:
-	fill(elizabethStats.battle_actions)
-	connect_scroll_children()
+signal resource_selected(resource)
+
+func grab_button_focus() -> void:
 	if button_container.get_child_count() > 0:
 		button_container.get_child(0).grab_focus()
 
@@ -19,6 +17,7 @@ func fill(resource_list : Array) -> void:
 		var resource_button : ResourceButton = add_resource_button()
 		resource_button.resource = resource
 		resource_button.text = resource.name
+	connect_scroll_children()
 		
 func add_resource_button() -> ResourceButton:
 	var resource_button : ResourceButton = ResourceButtonScene.instance()
@@ -54,4 +53,4 @@ func get_focused_scroll_amount() -> int:
 	return focused_scroll
 
 func _on_resource_selected(resource: Resource) -> void:
-	print(resource.name)
+	emit_signal("resource_selected", resource)
