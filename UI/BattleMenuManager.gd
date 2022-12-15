cd552bd6eea59b031f4d54691fc6147d71ecf3ef
apiotrowski255 <awesomea900@gmail.com>
2022-12-15 12:58:46 +1100

    battle menu manager implemented in battle scene

--- UI/BattleMenuManager.gd
+++ UI/BattleMenuManager.gd
@@ -1,6 +1,9 @@
 extends Control
 
+const RUN_BATTLE_ACTION = preload("res://BattleActions/RunBattleAction.tres")
+
 var elizabethStats : PlayerClassStats = ReferenceStash.elizabethStats
+var inventory : Inventory = ReferenceStash.inventory
 
 var uiStack := UIStack.new()
 
@@ -12,14 +15,18 @@ onready var item_list: PanelContainer = $"%ItemList"
 onready var context_menu: PanelContainer = $"%ContextMenu"
 onready var info_menu: PanelContainer = $"%InfoMenu"
 
+signal battle_menu_resource_selected(selected_resource)
+
 func _ready() -> void:
 	action_list.fill(elizabethStats.battle_actions)
 	item_list.fill(elizabethStats.inventory.items)
+
+func show_battle_menu() -> void:
 	yield(battle_menu.show_menu(), "completed")
 	uiStack.push(battle_menu)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel") and uiStack.uis.size() > 1:
 		uiStack.pop()
 
 
@@ -29,6 +36,8 @@ func _on_BattleMenu_menu_option_selected(option : int) -> void:
 			uiStack.push(action_list)
 		BattleMenu.ITEM:
 			uiStack.push(item_list)
+		BattleMenu.RUN:
+			emit_signal("battle_menu_resource_selected", RUN_BATTLE_ACTION)
 
 
 func _on_ActionList_resource_selected(resource : BattleAction) -> void:
@@ -42,7 +51,14 @@ func _on_ItemList_resource_selected(resource : Item) -> void:
 func _on_ContextMenu_option_selected(option : int) -> void:
 	match option:
 		ContextMenu.USE:
-			print("use")
+			uiStack.clear()
+			battle_menu.show()
+			battle_menu.hide_menu()
+			battle_menu.release_focus()
+			if selected_resource is Item:
+				inventory.remove_item(selected_resource)
+			emit_signal("battle_menu_resource_selected", selected_resource)
+
 		ContextMenu.INFO:
 			if selected_resource is Item or selected_resource is BattleAction:
 				info_menu.text = selected_resource.description
