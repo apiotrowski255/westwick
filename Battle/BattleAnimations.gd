d4c858096eaed1de1158fcc3956fc93adfd00c4a
apiotrowski255 <awesomea900@gmail.com>
2022-12-17 22:05:26 +1100

    fixed up projectile to hit target:

--- Battle/BattleAnimations.gd
+++ Battle/BattleAnimations.gd
@@ -2,15 +2,19 @@ extends Sprite
 class_name BattleAnimations
 
 onready var animation_player: AnimationPlayer = $AnimationPlayer
+onready var emission_point: Position2D = $EmissionPoint
 
 signal animation_finished
 
 func _ready() -> void:
 	animation_player.connect("animation_finished", self, "_on_animation_finished")
-	
+
+func get_emission_position() -> Vector2:
+	return emission_point.global_position
+
 func get_current_animation_length() -> float:
 	return animation_player.current_animation_length / animation_player.playback_speed
-	
+
 func play(animation_name : String) -> void:
 	assert(animation_player.has_animation(animation_name))
 	animation_player.play(animation_name)
