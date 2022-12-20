99da63a01324c46d3503c83648a32546223e8147
apiotrowski255 <awesomea900@gmail.com>
2022-12-20 21:41:35 +1100

    finishing up with projectile and ranged battle action classes

--- Projectiles/Fireball.gd
+++ Projectiles/Fireball.gd
@@ -1,4 +1,15 @@
 extends Projectile
 
-func _animate_explosion() -> void:
-	pass
+onready var animated_sprite: AnimatedSprite = $AnimatedSprite
+onready var flame: CPUParticles2D = $Flame
+onready var explosion: CPUParticles2D = $Explosion
+
+func _animate_collision() -> void:
+	animated_sprite.hide()
+#	flame.hide()
+	flame.emitting = false
+	explosion.emitting = true
+#	while explosion.emitting == true:
+#		yield(get_tree(), "idle_frame")
+	yield(get_tree().create_timer(flame.lifetime * .75), "timeout")
+	emit_signal("collision_animation_finished")
